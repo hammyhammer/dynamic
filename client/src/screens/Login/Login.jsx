00032a46fb586133bbc315deb2a8c98eed37d302
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-25 10:10:54 -0500

    Added some content to login page

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { loginUser } from '../../services/users';
 import { useNavigate } from 'react-router-dom';
 
-export default function Login() {
+export default function Login(props) {
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -21,14 +21,20 @@ export default function Login() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
-      <input type="password" value={password} onChange={(event) => setUsername(event.target.value)} />
-      <button>Login</button>
+    <div>
+      <h3>Welcome to Dynamic</h3>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+          <input type="password" value={password} onChange={(event) => setUsername(event.target.value)} />
+          <button>Login</button>
 
+
+        </form>
+      </div>
       <div>New here? Signup!</div>
-
+      <button>Register</button>
       <div>Continue as Guest</div>
-    </form>
+    </div>
   )
 }
