7672682ad01c78c13a08516ab3d83c4cfbdc6e5d
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-25 09:27:03 -0500

    installed react dependencies in wrong place

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000';
+const baseUrl = 'http://localhost:3001';
 
 export const api = axios.create({
   baseUrl: baseUrl
---
+++ client/src/services/movements.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function movements() {
+  return (
+    <div>movements</div>
+  )
+}
