b23fd58d35ecaae6111095c6ead75535832d4b9f
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-25 09:50:13 -0500

    setting up routes and login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import { verify } from './services/users';
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
 
-  useEffect(() => {
-    const getUser = async () => {
-      const user = await verify();
-      setCurrentUser(user);
-    }
-    getUser();
-  }, []);
+  // useEffect(() => {
+  //   const getUser = async () => {
+  //     const user = await verify();
+  //     setCurrentUser(user);
+  //   }
+  //   getUser();
+  // }, []);
 
-  const logout = () => {
-    localStorage.removeItem('authToken')
-    setCurrentUser(null)
-  }
+  // const logout = () => {
+  //   localStorage.removeItem('authToken')
+  //   setCurrentUser(null)
+  // }
 
   return (
 
     <div className="App">
-      test
+      <Routes>
+        <Route path="/" element={<div>This is the home page</div>} />
+        <Route path="/login" element={<div>Login in page</div>} />
+        <Route path="/register" element={<div>Register page</div>} />
+      </Routes>
+
     </div>
   );
 }
