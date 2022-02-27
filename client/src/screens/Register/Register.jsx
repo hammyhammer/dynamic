45a3298a6dde0e99743a69b71daa5571aa13126f
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-27 16:04:27 -0500

    styling register page

--- client/src/screens/Register/Register.jsx
+++ client/src/screens/Register/Register.jsx
@@ -25,11 +25,15 @@ export default function Register(props) {
 
   return (
     <div className={registercss.whole}>
+      <h3 className={registercss.header}>Create an Account</h3>
       <form onSubmit={handleSubmit}>
         <input className={registercss.username} type="text" value={username} placeholder="Username" onChange={(event) => setUsername(event.target.value)} />
+        <br />
         <input className={registercss.email} type="email" value={email} placeholder="Email" onChange={(event) => setEmail(event.target.value)} />
+        <br />
         <input className={registercss.password} type="password" value={password} placeholder="Password" onChange={(event) => setPassword(event.target.value)} />
-        <button>Register</button>
+        <br />
+        <button className={registercss.button}>Register</button>
 
 
 
