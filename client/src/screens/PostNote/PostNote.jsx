68b5995f8b3c36e2f9d1945bb31bbd470b8622f8
Hampton Jacobs <hamptonjacobs@Hannahs-Air.fios-router.home>
2022-02-26 22:55:24 -0500

    tenary operator for posting notes

--- client/src/screens/PostNote/PostNote.jsx
+++ client/src/screens/PostNote/PostNote.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { verify } from '../../services/users'
 
 export default function PostNote(props) {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
 
-  const token = localStorage.getItem("token")
+  const [currentUser, setCurrentUser] = useState(null)
+  useEffect(() => {
+    const getUser = async () => {
+      const user = await verify()
+      setCurrentUser(user)
+    }
+    getUser()
+  }, [])
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -17,7 +25,7 @@ export default function PostNote(props) {
 
   return (
     <div>
-      {token ?
+      {currentUser ?
         <form onSubmit={handleSubmit}>
           <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
           <input type="text" value={content} onChange={(e) => setContent(e.target.value)} />
