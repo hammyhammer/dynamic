aca9707265df8c8c5757aa03c5f854d4d43ebdf1
Hampton Jacobs <hammyjacobs@gmail.com>
2022-03-01 10:25:44 -0500

    Mobile for home page, footer, nav

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import footercss from './Footer.module.css'
 export default function Footer() {
   return (
     <div className={footercss.footer}>
-      {/* <a className={footercss.a} href="https://github.com/hammyhammer">
+      <a className={footercss.a} href="https://github.com/hammyhammer">
         <img src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/github_fpykxh.png" alt="Github Icon"
           className={footercss.git} />
       </a>
       <a className={footercss.a} href="https://www.linkedin.com/in/hampton-jacobs-a80821126/">
         <img src="https://res.cloudinary.com/briandanger/image/upload/v1568954107/linkedin_vnvo6s.png" alt="Linkedln Icon"
           className={footercss.link} />
-      </a> */}
+      </a>
     </div>
   )
 }
