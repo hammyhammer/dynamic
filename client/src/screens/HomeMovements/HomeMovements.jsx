e2c6249cacfdb015a68e237b37bbccc3b3766992
Hampton Jacobs <hammyjacobs@gmail.com>
2022-03-02 09:57:21 -0500

    retrying deployment

--- client/src/screens/HomeMovements/HomeMovements.jsx
+++ client/src/screens/HomeMovements/HomeMovements.jsx
@@ -12,7 +12,7 @@ export default function HomeMovements(props) {
             <Link className={homecss.mini_titles} to="movements/random">
               <p className={homecss.random_title}>Training Set of the Day</p>
               <img src="https://media.istockphoto.com/photos/still-life-shot-of-exercise-equipment-in-a-gym-picture-id1320144030?b=1&k=20&m=1320144030&s=170667a&w=0&h=zRPUtgV7QbKzBYJ-VOHwNvKxRsLgWm4Z4eMi2KToJpc="
-                alt="Image of squatrack"
+                alt="Squatrack"
                 className={homecss.default_training_img} />
             </Link>
 
@@ -21,7 +21,7 @@ export default function HomeMovements(props) {
             <Link className={homecss.mini_titles} to='movements/custom'>
               <p className={homecss.custom_title}>Customize Your Training</p>
               <img src="https://media.istockphoto.com/photos/gym-background-with-clipboard-mockup-notepad-and-fitness-equipment-picture-id1210692942?k=20&m=1210692942&s=612x612&w=0&h=b2pFEdARCXBpMVbFTse_kYctLioA011CXgt6b6Fgl6Y="
-                alt="Image of clipboard"
+                alt="Clipboard"
                 className={homecss.default_training_img}
               />
             </Link>
@@ -33,13 +33,13 @@ export default function HomeMovements(props) {
             <Link className={homecss.mini_titles} to="movements/random">
               <p className={homecss.random_title}>Training Set of the Day</p>
               <img src="https://media.istockphoto.com/photos/still-life-shot-of-exercise-equipment-in-a-gym-picture-id1320144030?b=1&k=20&m=1320144030&s=170667a&w=0&h=zRPUtgV7QbKzBYJ-VOHwNvKxRsLgWm4Z4eMi2KToJpc="
-                alt="Image of squatrack"
+                alt="Squatrack"
                 className={homecss.default_training_img} />
             </Link>
             <Link className={homecss.mini_titles} to='/register'>
               <p className={homecss.custom_title_guest}>Customize your Training (Available to Users only)</p>
               <img src="https://media.istockphoto.com/photos/gym-background-with-clipboard-mockup-notepad-and-fitness-equipment-picture-id1210692942?k=20&m=1210692942&s=612x612&w=0&h=b2pFEdARCXBpMVbFTse_kYctLioA011CXgt6b6Fgl6Y="
-                alt="Image of clipboard"
+                alt="Clipboard"
                 className={homecss.default_training_img} />
             </Link>
           </Layout>
