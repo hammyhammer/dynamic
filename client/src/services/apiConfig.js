8da12e2c11650ce7d1dafe6a354ebf42f7c7f3fd
Hampton Jacobs <hammyjacobs@gmail.com>
2022-03-02 10:03:38 -0500

    slight slash

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'production' ? 'https://dynamic-fitness-api.herokuapp.com/' : 'http://localhost:3000';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://dynamic-fitness-api.herokuapp.com' : 'http://localhost:3000';
 export const api = axios.create({
   baseURL: baseUrl
 });
