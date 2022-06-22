7acb53fc6de993305638a447f351176e3fd724e1
Hampton Jacobs <hammyjacobs@gmail.com>
2022-06-22 16:59:47 -0400

    minor typos

--- client/src/screens/PostNote/PostNote.jsx
+++ client/src/screens/PostNote/PostNote.jsx
@@ -30,7 +30,7 @@ export default function PostNote(props) {
             <form onSubmit={handleSubmit}>
               {/* <input className={postcss.title} type="text" value={title} placeholder="Entry Title" onChange={(e) => setTitle(e.target.value)} /> */}
               <input className={postcss.content} type="text" value={content} placeholder="How did you do today?" onChange={(e) => setContent(e.target.value)} />
-              <button className={postcss.button}>Potato</button>
+              <button className={postcss.button}>Save Entry</button>
             </form>
           </div>
           :
