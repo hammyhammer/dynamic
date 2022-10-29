a6eb0ec582fecfd2fd5275cfa0e145d91a145e0e
hammyhammer <hammyjacobs@gmail.com>
2022-10-29 10:06:18 -0400

    padding issue on ipad screen

--- client/src/screens/Notes/Notes.jsx
+++ client/src/screens/Notes/Notes.jsx
@@ -35,7 +35,7 @@ export default function Notes(props) {
                   </div>
                 </>
                 :
-                <div>test</div>
+                null
             }
           </div>
         ))
